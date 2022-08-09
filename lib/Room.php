72a3a4bd385cb007d3e39d8c7c4ac3f72cde71cc
Vitor Mattos <vitor@php.rio>
2022-08-09 17:14:34 -0300

    Rename field
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- lib/Room.php
+++ lib/Room.php
@@ -198,7 +198,7 @@ class Room {
 								int $readOnly,
 								int $listable,
 								int $messageExpiration,
-								bool $showHistory,
+								int $showHistory,
 								int $lobbyState,
 								int $sipEnabled,
 								?int $assignedSignalingServer,
