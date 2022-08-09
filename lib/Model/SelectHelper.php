22cd3639dd8b09d2b634495cb64d975da0c3ba23
Vitor Mattos <vitor@php.rio>
2022-08-09 17:14:34 -0300

    Rename field
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- lib/Model/SelectHelper.php
+++ lib/Model/SelectHelper.php
@@ -52,7 +52,7 @@ class SelectHelper {
 			->addSelect($alias . 'object_id')
 			->addSelect($alias . 'listable')
 			->addSelect($alias . 'message_expiration')
-			->addSelect($alias . 'pre_history')
+			->addSelect($alias . 'show_history')
 			->addSelect($alias . 'remote_server')
 			->addSelect($alias . 'remote_token')
 			->selectAlias($alias . 'id', 'r_id');
